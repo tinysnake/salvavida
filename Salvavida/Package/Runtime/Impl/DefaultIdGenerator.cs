7c534d0933a6fbc5f42cef5aae10d785a7e3607c
SNAKE <tff.assist@live.com>
2024-10-16 18:54:51 +0800

    DefaultIdGenerator improvement

--- Salvavida/Package/Runtime/Impl/DefaultIdGenerator.cs
+++ Salvavida/Package/Runtime/Impl/DefaultIdGenerator.cs
@@ -7,7 +7,7 @@ namespace Salvavida.DefaultImpl
         private static uint baseValue = (uint)DateTimeOffset.UtcNow.Ticks;
         public string GetId()
         {
-            return (++baseValue).ToString("x");
+            return ((uint)(16777259L * ++baseValue)).ToString("x8");
         }
     }
 }
